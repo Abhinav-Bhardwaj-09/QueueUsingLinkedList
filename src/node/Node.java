7e84ea51c5524470f9d45f2d0bc2c416a4c05e4d
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2021-08-03 21:37:52 +0530

    Changed the name of the next node in the Node Class.

--- src/node/Node.java
+++ src/node/Node.java
@@ -9,25 +9,25 @@ package node;
 
 public class Node {
     private int data;
-    private Node next;
+    private Node nextNode;
     public Node (int data) {
         this.data = data;
-        this.next = null;
+        this.nextNode = null;
     }
 
     public int getData() {
         return data;
     }
     public Node getNext() {
-        return next;
+        return nextNode;
     }
 
     public void setData(int data) {
         this.data = data;
     }
 
-    public void setNext(Node next) {
-        this.next = next;
+    public void setNext(Node nextNode) {
+        this.nextNode = nextNode;
     }
 
 
